7ba5be54adb29d9ae2d28d992113999074345c55
jomolrk <93908953+jomolrk@users.noreply.github.com>
2022-03-22 13:03:35 +0530

    Add files via upload

---
+++ 22-03-2022/Stri.java
@@ -0,0 +1,9 @@
+public class Stri
+{
+	public static void main(String args[])
+	{
+		
+		System.out.println("Hello.."+"How are you");
+	}
+
+}
---
+++ 22-03-2022/command.java
@@ -0,0 +1,11 @@
+public class command
+{
+	public static void main(String args[])
+		{
+			for(String a:args)
+			{
+				
+				System.out.println(a);
+			}
+		}
+}
